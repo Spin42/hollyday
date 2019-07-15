a1e509dffd2147e13d7726b5fc22316e3603f474
Marc Lainez <marc.lainez@gmail.com>
2019-07-15 23:12:27 +0200

    Be more error tolerant in the command

--- commands/wfh.rb
+++ commands/wfh.rb
@@ -7,9 +7,19 @@ class Wfh < SlackRubyBot::Commands::Base
   match(/WFH$*(?<expression>.*)$/)
 
   def self.call(client, data, _match)
-    wfh_day = _match[:expression].strip.split(" ")[0].try(:gsub,",","")
     team = Team.where(team_id: data.team).first
     webclient = Slack::Web::Client.new(token: team.token)
+
+    begin
+      wfh_day = _match[:expression].strip.split(" ")[0].try(:gsub,",","")
+    rescue Exception => e
+      self.post_ErrorMessage(
+        webclient, data.user, data.channel,
+        ":thinking_face: Please use today, tomorrow, or any day of the week..."
+      )
+      return
+    end
+
     case wfh_day
     when "summary"
       wfhs = Leave.where(
@@ -40,11 +50,10 @@ class Wfh < SlackRubyBot::Commands::Base
       begin
         day_of_the_week = Date.parse(wfh_day).cwday
       rescue Exception => e
-        webclient.chat_postEphemeral(
-          user: data.user,
-          channel: data.channel,
-          text: ":thinking_face: Please use today, tomorrow, or any day of the week...",
-          as_user: true)
+        self.post_ErrorMessage(
+          webclient, data.user, data.channel,
+          ":thinking_face: Please use today, tomorrow, or any day of the week..."
+        )
       else
         closest_day = self.closest_day(day_of_the_week)
         webclient.chat_postEphemeral(
@@ -105,5 +114,13 @@ class Wfh < SlackRubyBot::Commands::Base
       end
     end
     return attachments
+  end
+
+  def self.post_ErrorMessage(webclient, user, channel, message)
+    webclient.chat_postEphemeral(
+      user: user,
+      channel: channel,
+      text: message,
+      as_user: true)
   end
 end
