ae1cbe7a4b8b6637b224cdea815958ca17365291
Marc Lainez <marc.lainez@gmail.com>
2019-08-27 21:50:05 +0200

    Update help

--- commands/help.rb
+++ commands/help.rb
@@ -6,7 +6,7 @@ Commands
 
 help                           - get this helpful message
 wfh [today|tomorrow|monday...] - log when you are working from home
-pto|off from dd/mm to dd/mm    - log your personal time off
+pto from dd/mm to dd/mm        - log your personal time off
 summary                        - shows summary for next 10 calendar days
 
 ```
