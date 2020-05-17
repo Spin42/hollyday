3ff9ba3cb5adfaa52bb05c6aac61e45400374548
Marc Lainez <marc.lainez@gmail.com>
2020-05-17 22:17:18 +0200

    Fix afk help

--- commands/help.rb
+++ commands/help.rb
@@ -3,12 +3,13 @@ class Help < SlackRubyBot::Commands::Base
 ```
 Commands
 --------
-help                                - get this helpful message
-wfh [arguments]                     - log when you are working from home
-pto [arguments]                     - log your personal time off
-sick [arguments]                    - log your sick leave
-summary [wfh|pto|@user|month]       - shows summary for next 10 calendar days for type or user
-entries                             - list your entries and allows you to delete them
+help                                               - get this helpful message
+wfh [arguments]                                    - log when you are working from home
+pto [arguments]                                    - log your personal time off
+sick [arguments]                                   - log your sick leave
+afk until xx:xx or [arguments] from xx:xx to xx:xx - log your afk
+summary [wfh|pto|@user|month]                      - shows summary for next 10 calendar days for type or user
+entries                                            - list your entries and allows you to delete them
 
 [arguments] can be [tomorrow|today|monday|tuesday|...] or a single or range of dates [dd/mm]
 
